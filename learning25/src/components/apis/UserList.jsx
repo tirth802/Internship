80989f1e953526514edc899b36b83088226be80b
tirth802 <tirth202345@gmail.com>
2025-02-21 13:34:19 +0530

    toster

--- learning25/src/components/apis/UserList.jsx
+++ learning25/src/components/apis/UserList.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import { CustomLoader } from '../CustomLoader'
 import React, { useEffect, useState } from 'react'
+import { Bounce,toast,ToastContainer } from 'react-toastify'
 
 export const UserList = () => {
     const[user,setuser]=useState([])
-    const[isLoading,setisLoading]=useState(FinalizationRegistry)
+    const[isLoading,setisLoading]=useState(false)
     const getuserData=async()=>{
         //start
         setisLoading(true)
@@ -15,9 +16,49 @@ export const UserList = () => {
         //end
         setisLoading(false)
     }
-    useEffect(()=>{getuserData},[])
+    useEffect(()=>{getuserData()},[])
+    const deleteUser = async (id) => {
+        //alert(id)
+        const res = await axios.delete(
+          "https://node5.onrender.com/user/user/" + id
+        );
+        console.log(res);
+        if (res.status == 204) {
+          //alert("user deleted..");
+          toast.error('record deleted !!', {
+            position: "top-left",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Bounce,
+            });
+            getuserData(); //get -->
+        }
+        //const res = await axios.delete(`https://node5.onrender.com/user/user/${id}`)
+      };
   return (
     <div style={{textAlign:"center"}}>
+          <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+       {/* {
+            isLoading == true && <h1>Loading....</h1>
+        } */}
+        {isLoading == true && <CustomLoader/>}
         <h1>UserList</h1>
         {/* <button onClick={()=>{getuserData()}}>GET</button> */}
         <table className='table table-dark'>
@@ -28,6 +69,7 @@ export const UserList = () => {
                     <th>AGE</th>
                     <th>EMAIL</th>
                     <th>STATUS</th>
+                    <th>ACTION</th>
                 </tr>
             </thead>
             <tbody>
@@ -43,9 +85,9 @@ export const UserList = () => {
                                     user.isActive == true ? "Active" : "Not active"
                                 }
                             </td>
-                            <td>
-                                <button onClick={()=>{deletUser(user._id)}}className='btn btn-danger'>DELETE</button>
-                            </td>
+                             <td>
+                                <button onClick={()=>{deleteUser(user._id)}}className='btn btn-danger'>DELETE</button>
+                            </td> 
                         </tr>
                     })
                 }
